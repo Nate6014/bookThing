097f71036e6de704605e91386b3182997c37f359
Nate6014 <Nate6014@gmail.com>
2019-06-11 19:56:24 -0700

    made

---
+++ client/build/precache-manifest.f6a482eb7aa3e7aa0edeb56d60d55182.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9c45d0d2c9cc1cd4bb4e",
+    "url": "/static/js/main.e2aec593.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "9c45d0d2c9cc1cd4bb4e",
+    "url": "/static/css/main.029b88ce.chunk.css"
+  },
+  {
+    "revision": "ebd1fcccbc09a6d0632c49f831a9f008",
+    "url": "/index.html"
+  }
+];
